b8029c7bd1a159bdc6c0b6df34fdafb81fad4e52
Zaid <>
2020-10-03 17:15:12 -0700

    version 1.0

---
+++ app.js
@@ -0,0 +1,38 @@
+const inputValue = document.querySelector("#exampleEmailInput");
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let count = 3;
+const playAgain = document.createElement("button");
+playAgain.className = "button";
+playAgain.textContent = "Play Again";
+document.querySelector(".button-primary").addEventListener("click", (e) => {
+  count--;
+  console.log(randomNumber);
+  if (Number(inputValue.value) !== randomNumber && count === 0) {
+    document.querySelector(
+      ".message"
+    ).innerHTML = `<p style='color:red'> ${Number(
+      inputValue.value
+    )} is Wrong, Game Over</p>`;
+    document.querySelector("#exampleEmailInput").disabled = true;
+    document.querySelector(".button-primary").textContent = "Play Again";
+    document.querySelector(".button-primary").className = "button";
+  } else if (Number(inputValue.value) === randomNumber) {
+    document.querySelector(".message").innerHTML =
+      "<p style='color:teal'>Your Guess is Correct</p>";
+    document.querySelector("#exampleEmailInput").disabled = true;
+    document.querySelector(".button-primary").textContent = "Play Again";
+    document.querySelector(".button-primary").className = "button";
+  } else if (Number(inputValue.value) !== randomNumber) {
+    document.querySelector(
+      ".message"
+    ).innerHTML = `<p style='color:red'>${Number(
+      inputValue.value
+    )} is Wrong, You Have ${count} Guesses Left</p>`;
+  }
+});
+
+document.body.addEventListener("mousedown", (e) => {
+  if (e.target.className === "button") {
+    window.location.reload();
+  }
+});
